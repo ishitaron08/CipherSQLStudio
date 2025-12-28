ea8a19ec867470efa06d6b3a4dd921205b839ba1
ishit <ishitaron08@gmail.com>
2025-12-28 19:15:33 +0530

    Fix API URL to use Render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = 'http://localhost:5000/api'
+const API_BASE = import.meta.env.VITE_API_URL || 'https://cipher-sql-studio-api.onrender.com/api'
 
 // Create axios instance
 const api = axios.create({
